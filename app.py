4a55905fc03336a01e737ba2ce2bf26fe416c180
Zect3279 <zeck.3279@gmail.com>
2022-12-11 21:48:47 +0900

    wiki

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from gensim.models import doc2vec
+from gensim.models import word2vec
 import pandas as pd
 
 # 単語の分散表現を学習するために必要なデータを用意します。
@@ -8,7 +8,7 @@ import pandas as pd
 
 # 単語の分散表現を学習します。
 df = pd.read_csv('unwiki/unwiki_deploy.csv')
-model = doc2vec.Doc2Vec.load('unwiki/unwiki_model.model')
+model = word2vec.Word2Vec.load('unwiki/unwiki_model.model')
 
 
 col1, col2 = st.columns(2)
@@ -32,16 +32,4 @@ with col1:
 
 with col2:
   st.title("記事検索")
-  if word != "":
-    vector = model.infer_vector([word])
-    try:
-      similar_words2 = model.dv.most_similar([vector])
-    except:
-      st.write("他の単語を試してください")
-    else:
-      st.write("Similar articles:")
-      df_list = [df[df.id == k] for k, v in similar_words2]
-      for df in df_list:
-        st.markdown(f"[{str(df.title.values)[2:-2].replace('u3000', '')}](https://ansaikuropedia.org/index.php?curid={str(df.id.values)[1:-1]})")
-  else:
-    pass
+
